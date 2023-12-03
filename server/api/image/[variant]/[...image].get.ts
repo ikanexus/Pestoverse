11cde8bef5f7ea2de116532443a981feca284a6a
ikanexus <git+github@ika.gg>
2023-12-03 20:11:26 +0000

    feat: pass request headers to imagedelivery.net

--- server/api/image/[variant]/[...image].get.ts
+++ server/api/image/[variant]/[...image].get.ts
@@ -1,9 +1,22 @@
+const copyHeader = (event: any, res: Response, key: string) => {
+    const value = res.headers.get(key);
+    if (value) {
+        setResponseHeader(event, key, value);
+    }
+};
+
 export default defineEventHandler(async (event) => {
     const runtimeConfig = useRuntimeConfig(event);
     const variant = getRouterParam(event, "variant");
     const imageName = getRouterParam(event, "image");
-    const blob = await fetch(`https://imagedelivery.net/${runtimeConfig.cfImages.accountHash}/${imageName}/${variant}`);
-    const buffer = await blob.arrayBuffer();
-    setResponseHeader(event, "Cache-Control", "public, max-age=86400");
+
+    const { ...headers } = event.node.req.headers;
+
+    const res = await fetch(`https://imagedelivery.net/${runtimeConfig.cfImages.accountHash}/${imageName}/${variant}`, {
+        headers: headers as any,
+    });
+    const buffer = await res.arrayBuffer();
+    copyHeader(event, res, "content-type");
+    setResponseHeader(event, "cache-control", "public, max-age=3600");
     return Buffer.from(buffer);
 });
