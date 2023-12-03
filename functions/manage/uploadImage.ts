fea0ecccecf69811931651f1bcdcd23a035465b9
ikanexus <git+github@ika.gg>
2023-12-03 18:36:39 +0000

    feat: add image proxy pages function endpoint

---
+++ functions/manage/uploadImage.ts
@@ -0,0 +1,15 @@
+interface Env {
+    KV: KVNamespace;
+}
+
+export const onRequestPost: PagesFunction<Env> = async (context): Promise<Response> => {
+    const formData = await context.request.formData();
+    const file = formData.get("file");
+    const data = {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+    };
+
+    return new Response(JSON.stringify(data));
+};
