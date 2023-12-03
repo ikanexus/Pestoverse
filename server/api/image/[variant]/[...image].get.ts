bf7df832d7d8bf53a26ee0a92b76f77abb0846b0
ikanexus <git+github@ika.gg>
2023-12-03 19:52:00 +0000

    feat: add cache-control header in the hope cloudflare will start caching the response

--- server/api/image/[variant]/[...image].get.ts
+++ server/api/image/[variant]/[...image].get.ts
@@ -4,5 +4,6 @@ export default defineEventHandler(async (event) => {
     const imageName = getRouterParam(event, "image");
     const blob = await fetch(`https://imagedelivery.net/${runtimeConfig.cfImages.accountHash}/${imageName}/${variant}`);
     const buffer = await blob.arrayBuffer();
+    setResponseHeader(event, "Cache-Control", "public, max-age=86400");
     return Buffer.from(buffer);
 });
