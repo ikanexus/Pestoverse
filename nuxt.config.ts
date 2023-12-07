ea8c1eedcea16ed48a8253d9164dc2ab2a072e7e
ikanexus <git+github@ika.gg>
2023-12-07 18:04:55 +0000

    try jimp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,20 +41,4 @@ export default defineNuxtConfig({
             Poppins: [400, 500, 600, 700],
         },
     },
-    build: {
-        extend(config, { isDev, isClient }) {},
-    },
-    // build: {
-    //     transpile: ["@jsquash/png", "@jsquash/jpeg", "@jsquash/webp", "@jsquash/resize"],
-    // },
-    // vite: {
-    //     optimizeDeps: {
-    //         exclude: ["@jsquash/png", "@jsquash/jpeg", "@jsquash/webp", "@jsquash/resize"],
-    //     },
-    // },
-    nitro: {
-        experimental: {
-            wasm: true,
-        },
-    },
 });
--- server/api/manage/uploadImage.post.ts
+++ server/api/manage/uploadImage.post.ts
@@ -1,7 +1,5 @@
-import decodeJpeg, { init as initJpegWasm } from "@jsquash/jpeg/decode";
-import decodePng, { init as initPngWasm } from "@jsquash/png/decode";
-import encodeWebp, { init as initWebpWasm } from "@jsquash/webp/encode";
-import resize, { initResize } from "@jsquash/resize";
+import Jimp from "jimp";
+
 const SUPPORTED_FILETYPES = ["image/jpeg", "image/png"];
 
 type ImageResult = {
@@ -12,81 +10,28 @@ type ImageResult = {
     height: number;
 };
 
-const resizeImage = async (file: Buffer): Promise<ImageResult> => {
-    const webpEncWasm = await loadWasmInstance(() => import("@jsquash/webp/codec/enc/webp_enc_simd.wasm"));
-    const resizeWasm = await loadWasmInstance(() => import("@jsquash/resize/lib/resize/squoosh_resize_bg.wasm"));
-    const headers = {
-        Authorization: `Basic ${Buffer.from(`api:${tinifyToken}`).toString("base64")}`,
-        "Content-Type": "application/json",
-    };
-    const res = await fetch(`https://api.tinify.com/shrink`, {
-        method: "POST",
-        headers,
-        body: file,
-    });
-    const body = await res.json();
-    const output = body.output.url;
-    const width = body.output.width;
-    const height = body.output.height;
-
-    const shrunk = await fetch(output, {
-        method: "POST",
-        headers,
-        body: JSON.stringify({
-            convert: { type: "image/webp" },
-            resize: { method: "fit", width: 800, height: 800 },
-        }),
-    });
-
-    const placeholder = await fetch(output, {
-        method: "POST",
-        headers,
-        body: JSON.stringify({
-            convert: { type: "image/webp" },
-            resize: { method: "fit", width: 20, height: 20 },
-        }),
-    });
-    const shrunkImage = await shrunk.arrayBuffer();
-    const placeholderImage = await placeholder.arrayBuffer();
-    return { shrunk: Buffer.from(shrunkImage), placeholder: Buffer.from(placeholderImage), original: file, width, height };
+type DecodedImage = {
+    imageData: Jimp;
+    width: number;
+    height: number;
 };
 
-const toArrayBuffer = (buf: Buffer) => {
-    const ab = new ArrayBuffer(buf.length);
-    const view = new Uint8Array(ab);
-    for (let i = 0; i < buf.length; ++i) {
-        view[i] = buf[i];
-    }
-    return ab;
+const decodeImage = async (fileType: string, buffer: Buffer): Promise<DecodedImage> => {
+    const image = await Jimp.read(buffer);
+    const width = image.getWidth();
+    const height = image.getHeight();
+    console.log("width", width, "height", height);
+    return { imageData: image, width, height };
 };
 
-const decodeImage = async (fileType: string, buffer: ArrayBuffer): Promise<ImageData> => {
-    const jpegDecWasm = await loadWasmInstance(() => import("@jsquash/jpeg/codec/dec/mozjpeg_dec.wasm"));
-    const pngDecWasm = await loadWasmInstance(() => import("@jsquash/png/codec/squoosh_png_bg.wasm"));
-    console.log(buffer);
-    if (fileType === "image/jpeg") {
-        await initJpegWasm(jpegDecWasm);
-        const data = await decodeJpeg(buffer);
-        return data;
-    } else if (fileType === "image/png") {
-        await initPngWasm(pngDecWasm);
-        const data = await decodePng(buffer);
-        return data;
-    }
-
-    throw createError({ statusCode: 500, statusMessage: "Can't decode image" });
+const resizeImage = async (image: Jimp, width: number) => {
+    return image.clone().resize(width, Jimp.AUTO).getBufferAsync(Jimp.MIME_JPEG);
 };
 
 const uploadImages = async (data: ImageResult, bucket: any, name: string) => {
     await bucket.put(`full/${name}`, data.original);
     await bucket.put(`shrunk/${name}`, data.shrunk);
     await bucket.put(`placeholder/${name}`, data.placeholder);
-};
-
-const loadWasmInstance = async (importFn: () => Promise<WebAssembly.Module>, imports = {}) => {
-    const init = await importFn().then((wasm) => wasm.default || wasm);
-    const { instance } = await init(imports);
-    return instance;
 };
 
 export default defineEventHandler(async (event) => {
@@ -97,16 +42,12 @@ export default defineEventHandler(async (event) => {
         if (!SUPPORTED_FILETYPES.includes(fileType)) {
             throw createError({ statusCode: 400, statusMessage: "File type not supported" });
         }
-        const fileData = toArrayBuffer(file.data);
-        const imageData = await decodeImage(fileType, fileData);
+        const fileData = file.data;
+        const { imageData, width, height } = await decodeImage(fileType, fileData);
+        const shrunkImage = await resizeImage(imageData, 600);
 
-        console.log("imageData", imageData);
-        // const result = await resizeImage(file.data, runtimeConfig.tinifyToken);
-        // const name = "test-1.jpg";
-        // const { width, height } = result;
-        // console.log("width", width, "height", height);
-        // await uploadImages(result, event.context.cloudflare.env.IMAGES, name);
-        // return { success: true, width, height };
+        console.log("imageData", imageData, "width", width, "height", height);
+        console.log("shrunkImage", shrunkImage);
     }
     return {};
 });
